f3e9e99936ef1791705623e8a6d242041de72c15
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-20 13:51:46 +0530

    changes

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,9 +1,9 @@
 <?php
 //mysqli connection details
-define('global_mysqli_server', '');
-define('global_mysqli_user', '');
-define('global_mysqli_password', '');
-define('global_mysqli_database', '');
+define('global_mysqli_server', 'localhost');
+define('global_mysqli_user', 'root');
+define('global_mysqli_password', 'Popat#Panda@1234$');
+define('global_mysqli_database', 'nit');
 $dbconfig = new mysqli(global_mysqli_server,global_mysqli_user,global_mysqli_password,global_mysqli_database);
 
 if($dbconfig->connect_error)
@@ -14,7 +14,7 @@ if($dbconfig->connect_error)
 //table details
 define('global_mysqli_users_table','user_accounts');
 define('global_mysqli_departments_table','departments');
-define('global_mysqli_departments_table','departments');
+define('global_mysqli_forms_table','form_details');
 
 //other settings
 define('global_website_title','NIT From Portal');
