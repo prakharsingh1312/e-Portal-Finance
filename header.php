b2b33ed9ba37fa7b7c1bcb9c72157f28f15c1837
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-20 13:55:50 +0530

    made initial header

--- header.php
+++ header.php
@@ -1,10 +1,49 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+<nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="400">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="#">
+          National Institute of Technology Jalandhar
+        </a>
+		  </div>
+      <div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="./assets/img/blurred-image-1.jpg">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#new">
+              <i class="far fa-plus-square"></i>
+              <p> Submit a new Form</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#track" >
+              <i class="fas fa-question-circle"></i>
+              <p>Track submitted Form</p>
+            </a>
+          </li>
+			<li class="nav-item">
+            <a class="nav-link btn btn-neutral" href="#login" target="">
+              <i class="now-ui-icons arrows-1_share-66"></i>
+              <p>Admin Login</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
+              <i class="fab fa-twitter"></i>
+              <p class="d-lg-none d-xl-none">Twitter</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
+              <i class="fab fa-facebook-square"></i>
+              <p class="d-lg-none d-xl-none">Facebook</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
+              <i class="fab fa-instagram"></i>
+              <p class="d-lg-none d-xl-none">Instagram</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
