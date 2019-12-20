e4ea028ba327c2feddc74167e7a67679055f7c00
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-20 18:44:03 +0530

    made login

--- index.php
+++ index.php
@@ -26,10 +26,11 @@ include('assets/php/functions.php');
 	<?php include('header.php');
 	
 	?>
+		<div id="notification_div"><div id="notification_inner_div"><div id="notification_inner_cell_div"></div></div></div>
 		<div class="page-header clear-filter" filter-color="orange">
       <div class="page-header-image" data-parallax="true" style="background-image:url('./assets/img/header.jpg');">
       </div>
-			<div id="notification_div"><div id="notification_inner_div"><div id="notification_inner_cell_div"></div></div></div>
+			
 			<div class="wrapper"></div>
 		</div>
 		<footer class="footer">
