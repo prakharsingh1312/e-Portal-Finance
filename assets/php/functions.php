d1a7c4fa9085dd2652ede6370a088fe90c5d5f5e
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-20 19:24:08 +0530

    made login

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -13,7 +13,7 @@ include('config.php');
     function login($username,$password){
 			global $dbconfig;
 			$password=encrypt_password($password);
-            $stmt = $dbconfig->prepare("SELECT user_id, user_username, user_password FROM user_accounts WHERE username='?' AND password='?' LIMIT 1");
+            $stmt = $dbconfig->prepare("SELECT user_id, user_username, user_password FROM user_accounts WHERE username=? AND password=? LIMIT 1");
             $stmt->bind_param('ss',$username,$password);
             $stmt->execute();
             $result=$stmt->get_result();
