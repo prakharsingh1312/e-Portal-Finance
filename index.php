be28b0e10410f066b0228c74b5756bd8327ad638
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-20 14:50:33 +0530

    added initial jquery

--- index.php
+++ index.php
@@ -40,6 +40,11 @@ include('assets/php/functions.php');
                 National Institute of Technology, Jalandhar
               </a>
             </li>
+			</ul>
+		  </nav>
+			</div>
+		</footer>
+		
 		<script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
   <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
   <script src="./assets/js/core/bootstrap.min.js" type="text/javascript"></script>
