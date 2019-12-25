c947686705280ecea8a6b74da080c757a6ab8f04
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2019-12-25 12:32:05 +0530

    dashboard

--- now-ui-dashboard-master/examples/header.php
+++ now-ui-dashboard-master/examples/header.php
@@ -55,7 +55,7 @@
               <span class="navbar-toggler-bar bar3"></span>
             </button>
           </div>
-          <a class="navbar-brand" href="#pablo">Welcome to your Dashboard, _firstname_</a>
+          <a class="navbar-brand" href="#pablo">Hey _firstname_</a>
         </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-bar navbar-kebab"></span>
@@ -97,3 +97,6 @@
         </div>
       </div>
     </nav>
+    <div class="panel-header panel-header-lg">
+      <h2 align="center" class="d-sm-block" style="color:white;">WELCOME TO YOUR DASHBOARD</h2>
+    </div>
