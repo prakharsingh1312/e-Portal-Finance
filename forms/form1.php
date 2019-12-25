77ef812b1d85f34779efaa31cad263158073788d
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-26 02:35:06 +0530

    fix

--- forms/form1.php
+++ forms/form1.php
@@ -94,7 +94,7 @@
               <div class="form-group">
                 <label for="form1_from_date">From (DD/MM/YY) :</label>
 
-                <input type="text" name="from_date date-picker" id="form1_from_date">
+                <input type="text" name="from_date" id="form1_from_date">
               </div>
               <div class="form-group">
                 <label for="form1_to_date">To (DD/MM/YY) :</label>
@@ -333,7 +333,17 @@
   <!-- JS -->
   <script src="../assets/js/jquery.js"></script>
   <script src="../assets/js/main.js"></script>
-	  <script src="../assets/js/plugins/bootstrap-datepicker.js">
+		  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+  integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30="
+  crossorigin="anonymous"></script>
+	 <script>
+	  // INCLUDE JQUERY & JQUERY UI 1.12.1
+$( function() {
+	$( "#form1_from_date" ).datepicker({
+		dateFormat: "dd-mm-yy"
+		,	duration: "fast"
+	});
+} );</script> 
 
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
