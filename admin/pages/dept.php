3db572779eeb06acc292b363dd878a408b632eaf
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2019-12-26 02:16:28 +0530

    aaa

--- admin/pages/dept.php
+++ admin/pages/dept.php
@@ -117,6 +117,11 @@
           </button>
 
         </div>
+        <div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+  </div>
+</div>
       </div>
     </div>
   </div>
