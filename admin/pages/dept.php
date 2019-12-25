434fb766a4c4217c0ce409cda542f3b497b6d19a
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2019-12-26 02:40:23 +0530

    aaa

--- admin/pages/dept.php
+++ admin/pages/dept.php
@@ -40,7 +40,19 @@
           <span class="sr-only">Next</span>
         </a> -->
       </div>
-
+      <p>
+        <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          Link with href
+        </a>
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+          Button with data-target
+        </button>
+      </p>
+      <div class="collapse" id="collapseExample">
+        <div class="card card-body">
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+      </div>
 
       <div class="col-lg-4 d-none d-lg-block">
         <div class="card-counter success">
@@ -111,31 +123,6 @@
             </table>
           </div>
         </div>
-        <div align="center">
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-            Add Department
-          </button>
-
-        </div>
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
       </div>
     </div>
   </div>
