a90315839a987b73b89c79da15a7b54cc6f59fc4
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-26 02:08:06 +0530

    fix

--- forms/form1.php
+++ forms/form1.php
@@ -90,7 +90,7 @@
             <label for="form1_">7. Duration required for the event with date(S) :</label>
             <div class="form-row">
 
-              <div class="form-group">
+              <div class="form-group date">
                 <label for="form1_from_date">From (DD/MM/YY) :</label>
 
                 <input type="text" name="from_date" id="form1_from_date"><span class="input-group-addon"><i class="zmdi zmdi-calendar"></i></span>
@@ -332,6 +332,54 @@
   <!-- JS -->
   <script src="../assets/js/jquery.js"></script>
   <script src="../assets/js/main.js"></script>
+	  <script type="text/javascript">
+	   $(function () {
+   var bindDatePicker = function() {
+		$(".date").datetimepicker({
+        format:'YYYY-MM-DD',
+      inline:true,
+			icons: {
+				time: "fa fa-clock-o",
+				date: "fa fa-calendar",
+				up: "fa fa-arrow-up",
+				down: "fa fa-arrow-down"
+			}
+		}).find('input:first').on("blur",function () {
+			// check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
+			// update the format if it's yyyy-mm-dd
+			var date = parseDate($(this).val());
+
+			if (! isValidDate(date)) {
+				//create date based on momentjs (we have that)
+				date = moment().format('YYYY-MM-DD');
+			}
+
+			$(this).val(date);
+		});
+	}
+   
+   var isValidDate = function(value, format) {
+		format = format || false;
+		// lets parse the date to the best of our knowledge
+		if (format) {
+			value = parseDate(value);
+		}
+
+		var timestamp = Date.parse(value);
+
+		return isNaN(timestamp) == false;
+   }
+   
+   var parseDate = function(value) {
+		var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
+		if (m)
+			value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
+
+		return value;
+   }
+   
+   bindDatePicker();
+ });</script>
 
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
