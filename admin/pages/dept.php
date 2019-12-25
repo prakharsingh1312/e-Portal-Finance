4ca3c48082620d8eff19843108e8f9f127493b17
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2019-12-26 01:40:29 +0530

    aaa

--- admin/pages/dept.php
+++ admin/pages/dept.php
@@ -73,7 +73,7 @@
     <div class="col-md-12">
       <div class="card">
         <div class="card-header">
-          <h4 class="card-title"> Applications</h4>
+          <h4 class="card-title" style="margin-left:3%;"> Applications</h4>
         </div>
         <div class="card-body">
           <div class="table-responsive">
