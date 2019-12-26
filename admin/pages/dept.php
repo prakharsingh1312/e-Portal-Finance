1a74c3405647d327a65ebbcd87628552ec754951
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2019-12-26 23:46:57 +0530

    ab pakka chalega

--- admin/pages/dept.php
+++ admin/pages/dept.php
@@ -111,7 +111,7 @@
             </table>
           </div>
 
-          <button type="button" class="btn btn-primary float-right" data-toggle="modal" data-target="#exampleModal">
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
           Add Department
         </button>
         </div>
