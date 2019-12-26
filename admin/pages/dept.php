6dd65ddc47fc7d7d4c4a98f7375c174368c4631f
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2019-12-26 22:32:05 +0530

    ab pakka chalega

--- admin/pages/dept.php
+++ admin/pages/dept.php
@@ -110,30 +110,42 @@
               </tbody>
             </table>
           </div>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+          Add Department
+        </button>
         </div>
       </div>
     </div>
   </div>
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
+
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Add a New Department</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+        <form class="" action="index.html" method="post">
+          <div class="form-row">
+            <div class="form-group">
+              <label for="exampleInputEmail1">Department Name</label>
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Department Abbriviation</label>
+              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+          </div>
+        </form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Discard</button>
+        <button type="button" class="btn btn-primary">Add Department</button>
       </div>
     </div>
   </div>
