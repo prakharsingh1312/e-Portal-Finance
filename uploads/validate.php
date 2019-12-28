c215374b3fbda5a08c1d615fa3dd83b136b7942d
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-29 03:37:23 +0530

    Added file access restriction

--- uploads/validate.php
+++ uploads/validate.php
@@ -13,8 +13,16 @@ if (!isset($_SESSION['user_id'])) {
     $filename = basename($request_url);
     
     // Set headers
+	$ext = end((explode(".", $name)));
+	if($ext="pdf"){
     header('Content-type: application/pdf');
-    header('Content-Disposition: inline; filename='.$filename);
+    
+	}
+	else
+	{
+		header('Content-type: image/jpeg');
+	}
+	header('Content-Disposition: inline; filename='.$filename);
     
     // Output file content
     @readfile($document_root.$request_url);
