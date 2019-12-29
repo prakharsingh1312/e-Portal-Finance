b8ff3b4cdfe0b123bb59490819c02adc699bcd82
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-29 19:14:25 +0530

    Working on admin panel

--- admin/pages/dept.php
+++ admin/pages/dept.php
@@ -152,7 +152,7 @@ echo'
 <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
-      <div class="modal-content">
+      <div class="modal-content" id="modal-content">
         
       </div>
     </div>
