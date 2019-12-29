0cc7c42384f61d4b0ef957e29f8a21535d22f5e6
Prakhar Singh <prakharsingh13@gmail.com>
2019-12-30 01:06:14 +0530

    Working on admin panel

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -260,7 +260,7 @@ function show_departments(){
                   <td>'.$result1['department_abbreviation'].'</td>
                   <td>'.$result1['department_name'].'</td>
 				  <td class="text-right">
-                     <a href="" class="dept_show_edit_button" data-toggle="modal" data-target="#exampleModal" id="edit:'.$result1['department_id'].'">Edit</a> or <a href="" class="dept_show_delete_button" id="delete:'.$result['department_name'].':'.$result1['department_id'].'" data-toggle="modal" data-target="#exampleModal">Delete</a>
+                     <a href="" class="dept_show_edit_button" data-toggle="modal" data-target="#exampleModal" id="edit:'.$result1['department_id'].'">Edit</a> or <a href="" class="dept_show_delete_button" id="delete:'.$result1['department_name'].':'.$result1['department_id'].'" data-toggle="modal" data-target="#exampleModal">Delete</a>
                   </td>
                 </tr>';
                  } 
