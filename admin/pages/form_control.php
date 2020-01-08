1eff7d746f29f77e5eb68cfb992b80c4f1fecfc5
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2020-01-08 17:31:18 +0530

    form control

--- admin/pages/form_control.php
+++ admin/pages/form_control.php
@@ -167,7 +167,22 @@ elseif(isset($_GET['form_toggle'])){
 
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-danger btn-lg btn-block" style="float:left">Edit Guidelines</button>
+          <button type="button" class="btn btn-outline-info btn-lg btn-block" style="float:left">Edit Guidelines</button>
+          <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible Group Item #1
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div class="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Discard</button>
           <button type="button" class="btn btn-primary">Update Form</button>
           <!-- <button type="button" class="btn btn-primary">Save Department</button> -->
