3a72da16d250c01a2fb8236f11a5a26cafc3e513
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2020-01-08 17:56:54 +0530

    please

--- admin/pages/form_control.php
+++ admin/pages/form_control.php
@@ -178,7 +178,11 @@ elseif(isset($_GET['form_toggle'])){
 
         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
         <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably havent heard of them accusamus labore sustainable VHS.
+        <div class="form-group">
+  <label for="exampleFormControlTextarea2">Small textarea</label>
+  <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3"></textarea>
+</div>
+
         </div>
         </div>
         </div>
