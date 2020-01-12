bd5001e15edb927a8bd9d5444906ee1734975fa5
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2020-01-12 17:16:35 +0530

    pdf

--- pdfs/fpdf/forms/form1.php
+++ pdfs/fpdf/forms/form1.php
@@ -11,8 +11,10 @@ function Header()
 	// Arial bold 15
 	$this->SetFont('Arial','B',10);
 	// Move to the right
-	$this->Write(15,' DR. B. R. AMBEDKAR NATIONAL INSTITUTE OF TECHNOLOGY, JALANDHAR-144011');
-	
+	$this->Cell(80);
+	// Title
+	$this->Cell(0,10,' DR. B. R. AMBEDKAR NATIONAL INSTITUTE OF TECHNOLOGY, JALANDHAR-144011',0,0,'C');
+	// Line break
 	$this->Ln(20);
 }
 
