2a325d8e1bfd7974ce4a1ef62dac140c59e9ce9f
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2020-01-12 19:29:42 +0530

    pdf

--- pdfs/fpdf/forms/form1.php
+++ pdfs/fpdf/forms/form1.php
@@ -13,28 +13,11 @@ function Header()
 	// Arial bold 15
 	$this->SetFont('Arial','B',10);
 	// Move to the right
-	$this->Cell(20);
+	$this->Cell(10);
 	// Title
 	$this->Cell(0,7,' DR. B. R. AMBEDKAR NATIONAL INSTITUTE OF TECHNOLOGY, JALANDHAR-144011',0,0,'C');
 	// Line break
 	$this->Ln(5);
-}
-
-// Page footer
-function Footer()
-{
-	// Position at 1.5 cm from bottom
-//	$this->SetY(-35);
-//	$html='<table border="1">
-//	<tr>
-//	<td width="200" height="30">cell 1</td><td width="200" height="30" bgcolor="#D0D0FF">cell 2</td>
-//	</tr>
-//	<tr>
-//	<td width="200" height="30">cell 3</td><td width="200" height="30">cell 4</td>
-//	</tr>
-//	</table>';
-//
-//	$this->WriteHTML($html);
 }
 }
 
@@ -44,17 +27,8 @@ $pdf = new PDFS();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Times','',12);
-for($i=1;$i<=40;$i++)
-	$pdf->Cell(0,10,'Printing line number '.$i,0,1);
-	$html='<table border="1">
-	<tr>
-	<td width="200" height="30">cell 1</td><td width="200" height="30" bgcolor="#D0D0FF">cell 2</td>
-	</tr>
-	<tr>
-	<td width="200" height="30">cell 3</td><td width="200" height="30">cell 4</td>
-	</tr>
-	</table>';
-
-	$pdf->WriteHTML($html);
+$pdf->cell(15);
+$pdf->Cell(0,7,'PROPOSAL FOR ATTENDING NATIONAL CONFERENCE / TRAINING COURSES / OTHERS UNDER',0,1,'C');
+$pdf->Cell(0,7,'TEQIP-II Annexure “A”',0,1,'C');
 $pdf->Output();
 ?>
