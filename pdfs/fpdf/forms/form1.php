2573f8eb56bbf93fd1d92d1da3f40bfbc4d5552c
utkarsh chauhan <43794187+uchauhan2022@users.noreply.github.com>
2020-01-13 13:28:29 +0530

    pdf

--- pdfs/fpdf/forms/form1.php
+++ pdfs/fpdf/forms/form1.php
@@ -96,7 +96,7 @@ $pdf->cell(170,7,'19. CGPA of the candidate (Only for UG / M.Tech and M.Sc stude
 $pdf->cell(10,7,'',0,0);
 $pdf->MultiCell(170,4,'20. For M.Tech, M.Sc and PhD candidates : Whether one paper has been accepted for publication in SCI Journal in Last two years (State Yes / No and attach the acceptance letter): ',1,1);
 
-$pdf->SetY(-45);
+$pdf->SetY(-60);
 
 $pdf->cell(10,7,'',0,0);
 $pdf->cell(50,7,'20. Signature of the student','TLR',0);
@@ -106,7 +106,7 @@ $pdf->MultiCell(70,7,'22. Recommended/ Not recommended by HoD','TLR',1);
 $pdf->cell(10,7,'',0,0);
 $pdf->cell(50,15,'','BLR',0);
 $pdf->cell(50,15,'','BLR',0);
-$pdf->MultiCell(70,15,'
+$pdf->MultiCell(70,7.5,'
 
 Signature (with seal) of the HoD:','BLR',1);
 
