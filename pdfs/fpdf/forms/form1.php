3d35089a44330dd3fa2df1742607532e41196734
Prakhar Singh <prakharsingh13@gmail.com>
2020-05-14 20:17:38 +0530

    fix

--- pdfs/fpdf/forms/form1.php
+++ pdfs/fpdf/forms/form1.php
@@ -2,6 +2,9 @@
 require('../fpdf.php');
 require('../fpdf_merge.php');
 require('../../../assets/php/functions.php');
+function url($data){
+	return "File Attached.";
+}
 if(isset($_GET['id'])){
 	$result=render_form1_pdf($_GET['id']);
 
@@ -37,72 +40,72 @@ $pdf->ln(15);
 
 //start table
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'1. Name of the Student(Full time):'.$result['name_of_student'],1,1);
+$pdf->cell(170,7,'1. Name of the Student(Full time): '.$result['name_of_student'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'1.1 Course:',1,1);
+$pdf->cell(170,7,'1.1 Course: '.$result['course'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'2. Roll Number:',1,1);
+$pdf->cell(170,7,'2. Roll Number: '.$result['roll_no'],1,1);
 //$pdf->cell(5,7,'3. Department:',1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'3. Department:',1,1);
+$pdf->cell(170,7,'3. Department: '.$result['department'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'4. Nature of Event:',1,1);
+$pdf->cell(170,7,'4. Nature of Event: '.$result['nature_of_event'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'5. Name of Event:',1,1);
+$pdf->cell(170,7,'5. Name of Event: '.$result['name_of_event'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->MultiCell(170,4,'6. Place (S) of the event (Visit) Conference /  Training Course /  Workshop Seminar / Symposium / Others (Complete Address): ',1,1);
+$pdf->MultiCell(170,4,'6. Place (S) of the event (Visit) Conference /  Training Course /  Workshop Seminar / Symposium / Others (Complete Address): '.$result['place_of_event'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'7. Duration required for the event with date (S) : From______________To ______________No. Of days:___',1,1);
+$pdf->cell(170,7,'7. Duration required for the event with date (S) : From '.$result['duration_from'].' To '.$result['duration_to'].' No. Of days: '.$result['duration_days'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'8. Organizer of the event:',1,1);
+$pdf->cell(170,7,'8. Organizer of the event: '.$result['organizer_of_event'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'9. Relevance of the visit / training: (Attach Separate Sheet, if required)',1,1);
+$pdf->cell(170,7,'9. Relevance of the visit / training: (Attach Separate Sheet, if required) '.($result['relevance_file']=="Y"?url($result['relevance_of_visit']):$result['relevance_of visit']),1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'10. Clear objective and outcome of the visit: (Attach Separate Sheet, if required)',1,1);
+$pdf->cell(170,7,'10. Clear objective and outcome of the visit: (Attach Separate Sheet, if required)'.($result['objective_file']=="Y"?url($result['objective_of_visit']):$result['objective_of visit']),1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'11. Attach (i) brief CV / biography ',1,1);
+$pdf->cell(170,7,'11. Attach (i) brief CV / biography : '.url($result['attached_cv']),1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'12. Attach certificate from HoD regarding relevance of the event for the applicant and by stating that the event will benefit for the applicant',1,1);
+$pdf->cell(170,7,'12. Attach certificate from HoD regarding relevance of the event for the applicant and by stating that the event will benefit for the applicant:  '.url($result['attached_certificate_hod']),1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'13. Date and time of departure from the Institute:',1,1);
+$pdf->cell(170,7,'13. Date and time of departure from the Institute: '.$result['date_and_time_departure'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'14. Date and time of arrival from the Institute:',1,1);
+$pdf->cell(170,7,'14. Date and time of arrival from the Institute: '.$result['date_and_time_arrival'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'15.  Whether going to present research paper :',1,1);
+$pdf->cell(170,7,'15.  Whether going to present research paper : '.$result['research_paper'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'16. Title of Paper',1,1);
+$pdf->cell(170,7,'16. Title of Paper:  '.$result['title_of_paper'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'17. Attach the accepted paper, acceptance letter, NOC from co-authors:',1,1);
+$pdf->cell(170,7,'17. Attach the accepted paper, acceptance letter, NOC from co-authors: '.($result['research_paper']=="Yes"?url($result['attach_paper_acceptance_letter']):$result['attach_paper_acceptance_letter']),1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->MultiCell(170,4,'18. Total cost involved Rs. ______________________ (Rupees ___________________ Only)
-Please give cost details (Attach separate sheet , if necessary)
-Registration Fee:_________________
-TA: ________________
-Others, If any (Special)',1,1);
+$pdf->MultiCell(170,4,'18. Total cost involved Rs. '.$result['total_cost'].' (Rupees '.$result['total_cost_words'].' Only)
+Please give cost details (Attach separate sheet , if necessary) '.($result['cost_details_file']=="Y"?url($result['cost_details']):$result['cost_details']).'
+Registration Fee:'.$result['registration_fees'].'
+TA: '.$result['transportation_allowance'].'
+Others, If any (Special): '.$result['other_costs'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->cell(170,7,'19. CGPA of the candidate (Only for UG / M.Tech and M.Sc students) : ',1,1);
+$pdf->cell(170,7,'19. CGPA of the candidate (Only for UG / M.Tech and M.Sc students) : '.$result['cgpa'],1,1);
 
 $pdf->cell(10,7,'',0,0);
-$pdf->MultiCell(170,4,'20. For M.Tech, M.Sc and PhD candidates : Whether one paper has been accepted for publication in SCI Journal in Last two years (State Yes / No and attach the acceptance letter): ',1,1);
+$pdf->MultiCell(170,4,'20. For M.Tech, M.Sc and PhD candidates : Whether one paper has been accepted for publication in SCI Journal in Last two years (State Yes / No and attach the acceptance letter): '.$result['sci_journal'],1,1);
 
 $pdf->SetY(-50);
 
