23e184e42d8c00f7a96eb8992192bf1a9ca98253
Utkarsh Chauhan <utkarsh@Utkarshs-MacBook-Pro.local>
2020-05-24 00:05:39 +0530

    m

--- admin/pages/dash.php
+++ admin/pages/dash.php
@@ -576,26 +576,27 @@ else{
 
 
 	<!-- Modal -->
-	  <div class="modal fade" id="actions" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-	    <div class="modal-dialog" role="document">
-	        <div class="modal-body">
-					<p>
-					  <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-					    Link with href
-					  </a>
-					  <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-					    Button with data-target
-					  </button>
-					</p>
-					<div class="collapse" id="collapseExample">
-					  <div class="card card-body">
-					    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-					  </div>
-					</div>
-	      <div class="modal-content" id="modal-content">
+	<div class="modal fade" id="actions" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        ...
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Save changes</button>
+	      </div>
 	    </div>
 	  </div>
 	</div>
-';
+
+
+	 ';
 }
 ?>
