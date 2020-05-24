51b26f455e347b31355c28052914caaa63c91129
Prakhar Singh <prakharsingh13@gmail.com>
2020-05-24 21:03:31 +0530

    fix

--- user/index.php
+++ user/index.php
@@ -215,9 +215,15 @@ if (!logged_in()){
     $(document).ready(function() {
       // Javascript method's body can be found in assets/js/demos.js
       md.initDashboardPageCharts();
-
+showDashStu();
+	$(window).bind('hashchange', function ()
+		{
+			hash();
+		});
+	hash();
     });
   </script>
+	
 </body>
 
 </html>
