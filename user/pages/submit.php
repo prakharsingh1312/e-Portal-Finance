cdb32f1478c5bfb7ae039d4368caafd792a9cdd2
Utkarsh Chauhan <utkarsh@Utkarshs-MacBook-Pro.local>
2020-05-25 14:30:55 +0530

    m

--- user/pages/submit.php
+++ user/pages/submit.php
@@ -2,7 +2,7 @@
   include('../../assets/php/functions.php');
 if(isset($_GET['dept'])){
 	$dept_id=mysqli_real_escape_string($dbconfig,$_POST['dept_id']);
-	get_forms_temp($dept_id);
+	echo get_forms_temp($dept_id);
 }
 else
   echo get_forms_temp(0);
