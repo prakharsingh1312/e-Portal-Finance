d09aeeecdc782c89c7c316bfd7f6270379d0ad65
Utkarsh Chauhan <utkarsh@Utkarshs-MacBook-Pro.local>
2020-05-25 09:25:01 +0530

    m

--- user/pages/header.php
+++ user/pages/header.php
@@ -75,15 +75,6 @@
             <span class="navbar-toggler-icon icon-bar"></span>
           </button>
           <div class="collapse navbar-collapse justify-content-end">
-            <form class="navbar-form">
-              <div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Search...">
-                <button type="submit" class="btn btn-default btn-round btn-just-icon">
-                  <i class="material-icons">search</i>
-                  <div class="ripple-container"></div>
-                </button>
-              </div>
-            </form>
             <ul class="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link" href="javascript:void(0)">
@@ -107,9 +98,9 @@
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="javascript:void(0)">
-                  <i class="material-icons">person</i>
+                  <i class="material-icons">logout</i>
                   <p class="d-lg-none d-md-block">
-                    Account
+                    Logout
                   </p>
                 </a>
               </li>
