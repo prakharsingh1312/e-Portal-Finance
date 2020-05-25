6f016b51a3529ee4f789609fa794a8a1c80d290a
Utkarsh Chauhan <utkarsh@Utkarshs-MacBook-Pro.local>
2020-05-25 09:25:50 +0530

    m

--- user/pages/header.php
+++ user/pages/header.php
@@ -76,18 +76,11 @@
           </button>
           <div class="collapse navbar-collapse justify-content-end">
             <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="javascript:void(0)">
-                  <i class="material-icons">dashboard</i>
-                  <p class="d-lg-none d-md-block">
-                    Stats
-                  </p>
-                </a>
-              </li>
+
               <li class="nav-item dropdown">
                 <a class="nav-link" href="javscript:void(0)" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <i class="material-icons">notifications</i>
-                  <span class="notification">5</span>
+                  <span class="notification">1</span>
                   <p class="d-lg-none d-md-block">
                     Some Actions
                   </p>
